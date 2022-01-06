01c832968c2560bb0ec19d14b03c20fdacb6a959
Nicolas de Garrigues <ndegarrigues@gmail.com>
2022-01-06 09:16:00 +0100

    :sparkles: FEAT add crud

---
+++ server/controllers/UserController.js
@@ -0,0 +1,111 @@
+var Userdb = require('../models/User')
+
+// create and save new user
+exports.create = (req, res) => {
+    // validate req
+    if(!req.body) {
+        res.status(400).send('Request body is missing')
+        return
+    }
+
+    // create new user
+    const user = new Userdb({
+        name: req.body.name,
+        email: req.body.email,
+        gender: req.body.gender,
+        status: req.body.status
+    })
+
+    // save user in db
+    user
+        .save(user)
+        .then((result) => {
+            // res.send(result)
+            res.redirect('/add-user')
+        })
+        .catch((err) => {
+            res.status(500).send({
+                message: err.message || 'Some error occurred while creating the user'
+            })
+        });
+}
+
+// retrieve all users / retrieve single user
+exports.find = (req, res) => {
+
+    if(req.query.id) {
+        const id = req.query.id
+        Userdb.findById(id)
+            .then(data => {
+                if(!data) {
+                    res.status(404).send({
+                        message: `User with id ${id} not found`
+                    })
+                } else {
+                    res.send(data)
+                }
+            })
+            .catch((err) => {
+                res.status(500).send({
+                    message: err.message || `Error retrieving user with id ${id}`
+                })
+            })
+    } else {
+        Userdb.find()
+            .then((users) => {
+                res.send(users)
+            })
+            .catch((err) => {
+                res.status(500).send({
+                    message: err.message || 'Some error occurred while retrieving users'
+                })
+            })
+    }
+}
+
+// update user
+exports.update = (req, res) => {
+    if(!req.body) {
+        res.status(400).send('Request body is missing')
+        return
+    }
+
+    const id = req.params.id
+    Userdb.findByIdAndUpdate(id, req.body, {useFindAndModify: false})
+    .then(data => {
+        if(!data) {
+            res.status(404).send({
+                message: `Cannot update user with id ${id}. User was not found`
+            })
+        } else {
+            res.send(data)
+        }
+    })
+    .catch((err) => {
+        res.status(500).send({
+            message: `Error updating user with id ${id}`
+        })
+    })
+}
+
+// delete user
+exports.delete = (req, res) => {
+    const id = req.params.id
+    Userdb.findByIdAndDelete(id, {useFindAndModify: false})
+    .then(data => {
+        if(!data) {
+            res.status(404).send({
+                message: `Cannot delete user with id ${id}. User was not found`
+            })
+        } else {
+            res.send({
+                message: `User with id ${id} was deleted`
+            })
+        }
+    })
+    .catch((err) => {
+        res.status(500).send({
+            message: `Error deleting user with id ${id}`
+        })
+    })
+}
---
+++ server/database/connection.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const connectDB = async() => {
+    try {
+        // mongodb connection string
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+
+        console.log(`MongoDB Connected: ${conn.connection.host}`)
+    } catch(e) {
+        console.log(e)
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB
