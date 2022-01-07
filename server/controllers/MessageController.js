bd65472d365a70447f7b2f07540247cff563145e
Bakabruh <chewie.louveau@gmail.com>
2022-01-07 10:02:42 +0100

    add message controller

---
+++ server/controllers/MessageController.js
@@ -0,0 +1,22 @@
+let Messagedb = require('../models/Message');
+
+exports.create = (res, req) => {
+    if(!req.body) {
+        res.status(400).send('Request body is missing !')
+        return
+    }
+
+    // Create message
+    const message = new Messagedb({
+        userId: req.body.userId,
+        content: req.body.content
+    })
+
+    message.save(message).then((result) => {
+        res.send(result)
+    }).catch((err) => {
+        res.status(500).send({
+            message: err.message || "An error has occurred"
+        })
+    })
+}
